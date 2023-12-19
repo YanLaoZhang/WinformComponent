492f66f377ad32b7ec46f2a760a50884e44b4646
Ethan <ethan.yan@d-3.com.hk>
2023-12-19 19:40:30 +0800

    Fix：添加AForge截图版组件 Issue:NA

---
+++ AForge/AForgeForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace AForge
+{
+    partial class AForgeForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1406, 834);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AForgeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1406, 834);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "AForgeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AForge展示摄像头画面";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AForgeForm_FormClosing);
+            this.Load += new System.EventHandler(this.AForgeForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AForge/AForgeForm.cs
@@ -0,0 +1,129 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AForge
+{
+    public partial class AForgeForm : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+
+        public AForgeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AForgeForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string camera = "iCatch V37";
+                // 获取所有摄像头
+                FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                int index = -1;
+                for (int i = 0; i < videoDevices.Count; i++)
+                {
+                    if (videoDevices[i].Name.Contains(camera))
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index == -1)
+                {
+                    MessageBox.Show($"未找到摄像头{camera}");
+                    this.Close();
+                }
+                videoSource = new VideoCaptureDevice(videoDevices[index].MonikerString);
+
+                // Get available video resolutions for the selected device
+                var resolutions = videoSource.VideoCapabilities.Select(cap => cap.FrameSize);
+
+                // Choose the highest resolution
+                var highestResolution = resolutions.OrderByDescending(res => res.Width * res.Height).FirstOrDefault();
+
+                if (highestResolution != null)
+                {
+                    // Set the video resolution
+                    videoSource.VideoResolution = videoSource.VideoCapabilities
+                        .FirstOrDefault(cap => cap.FrameSize == highestResolution);
+                    Console.WriteLine($"{highestResolution}");
+                    // Register event handler for new frames
+                    videoSource.NewFrame += VideoSource_NewFrame;
+
+                    // Start capturing
+                    videoSource.Start();
+                }
+                else
+                {
+                    MessageBox.Show("No supported resolutions found.");
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show($"开启摄像头，发生异常:{ee.Message}");
+            }
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            var height = eventArgs.Frame.Height;
+            var width = eventArgs.Frame.Width;
+            Console.WriteLine($"分辨率：w[{width}]-h[{height}]");
+            pictureBox1.Image = (System.Drawing.Image)eventArgs.Frame.Clone();
+
+            GC.Collect();  //强制GC来恢复
+        }
+
+        public Bitmap CapturePNG(string Path_image_file)
+        {
+            try
+            {
+                if (pictureBox1.Image != null)
+                {
+                    Bitmap img = (Bitmap)pictureBox1.Image.Clone();
+                    if (img != null)
+                    {
+                        img.Save(Path_image_file, System.Drawing.Imaging.ImageFormat.Png);
+                        return img;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("摄像头未打开，无法截图");
+                    return null;
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show($"截图发生异常{ee.Message}");
+                return null;
+            }
+        }
+
+
+        private void AForgeForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource.NewFrame -= VideoSource_NewFrame;
+                Console.WriteLine($"已释放资源");
+            }
+        }
+    }
+}
