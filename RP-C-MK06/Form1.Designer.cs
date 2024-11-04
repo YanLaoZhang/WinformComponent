b4577ef50c0e184e04657cbd76af9f3ae3a409df
Ethan <ethan.yan@d-3.com.hk>
2024-11-04 18:25:43 +0800

    Feat： 新增RP-C-MK06压力传感器开发板串口组件 Issue:NA

---
+++ RP-C-MK06/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace RP_C_MK06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+            this.BtnOpenPort = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.BtnReadADC = new System.Windows.Forms.Button();
+            this.numericUpDownDuration = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.BtnClosePort = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxADC1Min = new System.Windows.Forms.TextBox();
+            this.textBoxADC1Max = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxADC2Min = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxADC2Max = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxADC1HFV = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxADC2HFV = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDuration)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "串口号:";
+            // 
+            // comboBoxSerialPort
+            // 
+            this.comboBoxSerialPort.FormattingEnabled = true;
+            this.comboBoxSerialPort.Location = new System.Drawing.Point(91, 10);
+            this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+            this.comboBoxSerialPort.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxSerialPort.TabIndex = 1;
+            this.comboBoxSerialPort.DropDown += new System.EventHandler(this.comboBoxSerialPort_DropDown);
+            // 
+            // BtnOpenPort
+            // 
+            this.BtnOpenPort.Location = new System.Drawing.Point(227, 8);
+            this.BtnOpenPort.Name = "BtnOpenPort";
+            this.BtnOpenPort.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpenPort.TabIndex = 2;
+            this.BtnOpenPort.Text = "打开串口";
+            this.BtnOpenPort.UseVisualStyleBackColor = true;
+            this.BtnOpenPort.Click += new System.EventHandler(this.BtnOpenPort_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBoxLog);
+            this.groupBox1.Location = new System.Drawing.Point(13, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 187);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口内容：";
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBoxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxLog.Location = new System.Drawing.Point(3, 17);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBoxLog.Size = new System.Drawing.Size(414, 167);
+            this.richTextBoxLog.TabIndex = 0;
+            this.richTextBoxLog.Text = "";
+            // 
+            // BtnReadADC
+            // 
+            this.BtnReadADC.Location = new System.Drawing.Point(605, 58);
+            this.BtnReadADC.Name = "BtnReadADC";
+            this.BtnReadADC.Size = new System.Drawing.Size(75, 23);
+            this.BtnReadADC.TabIndex = 4;
+            this.BtnReadADC.Text = "读取ADC值";
+            this.BtnReadADC.UseVisualStyleBackColor = true;
+            this.BtnReadADC.Click += new System.EventHandler(this.BtnReadADC_Click);
+            // 
+            // numericUpDownDuration
+            // 
+            this.numericUpDownDuration.Location = new System.Drawing.Point(521, 59);
+            this.numericUpDownDuration.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDownDuration.Name = "numericUpDownDuration";
+            this.numericUpDownDuration.Size = new System.Drawing.Size(78, 21);
+            this.numericUpDownDuration.TabIndex = 5;
+            this.numericUpDownDuration.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "持续时间：";
+            // 
+            // BtnClosePort
+            // 
+            this.BtnClosePort.Location = new System.Drawing.Point(308, 8);
+            this.BtnClosePort.Name = "BtnClosePort";
+            this.BtnClosePort.Size = new System.Drawing.Size(75, 23);
+            this.BtnClosePort.TabIndex = 7;
+            this.BtnClosePort.Text = "关闭串口";
+            this.BtnClosePort.UseVisualStyleBackColor = true;
+            this.BtnClosePort.Click += new System.EventHandler(this.BtnClosePort_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(450, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ADC1 Min：";
+            // 
+            // textBoxADC1Min
+            // 
+            this.textBoxADC1Min.Location = new System.Drawing.Point(521, 96);
+            this.textBoxADC1Min.Name = "textBoxADC1Min";
+            this.textBoxADC1Min.Size = new System.Drawing.Size(100, 21);
+            this.textBoxADC1Min.TabIndex = 9;
+            // 
+            // textBoxADC1Max
+            // 
+            this.textBoxADC1Max.Location = new System.Drawing.Point(521, 123);
+            this.textBoxADC1Max.Name = "textBoxADC1Max";
+            this.textBoxADC1Max.Size = new System.Drawing.Size(100, 21);
+            this.textBoxADC1Max.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(450, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ADC1 Max：";
+            // 
+            // textBoxADC2Min
+            // 
+            this.textBoxADC2Min.Location = new System.Drawing.Point(521, 190);
+            this.textBoxADC2Min.Name = "textBoxADC2Min";
+            this.textBoxADC2Min.Size = new System.Drawing.Size(100, 21);
+            this.textBoxADC2Min.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(450, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ADC2 Min：";
+            // 
+            // textBoxADC2Max
+            // 
+            this.textBoxADC2Max.Location = new System.Drawing.Point(521, 217);
+            this.textBoxADC2Max.Name = "textBoxADC2Max";
+            this.textBoxADC2Max.Size = new System.Drawing.Size(100, 21);
+            this.textBoxADC2Max.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(450, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "ADC2 Max：";
+            // 
+            // textBoxADC1HFV
+            // 
+            this.textBoxADC1HFV.Location = new System.Drawing.Point(575, 150);
+            this.textBoxADC1HFV.Name = "textBoxADC1HFV";
+            this.textBoxADC1HFV.Size = new System.Drawing.Size(100, 21);
+            this.textBoxADC1HFV.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(450, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "ADC1 MostFrequent：";
+            // 
+            // textBoxADC2HFV
+            // 
+            this.textBoxADC2HFV.Location = new System.Drawing.Point(575, 244);
+            this.textBoxADC2HFV.Name = "textBoxADC2HFV";
+            this.textBoxADC2HFV.Size = new System.Drawing.Size(100, 21);
+            this.textBoxADC2HFV.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(450, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(119, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "ADC2 MostFrequent：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 297);
+            this.Controls.Add(this.textBoxADC2HFV);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxADC1HFV);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxADC2Max);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxADC2Min);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxADC1Max);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxADC1Min);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnClosePort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownDuration);
+            this.Controls.Add(this.BtnReadADC);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnOpenPort);
+            this.Controls.Add(this.comboBoxSerialPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RP-C-MK06调试工具";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDuration)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxSerialPort;
+        private System.Windows.Forms.Button BtnOpenPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.Button BtnReadADC;
+        private System.Windows.Forms.NumericUpDown numericUpDownDuration;
+        private System.Windows.Forms.Label label2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button BtnClosePort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxADC1Min;
+        private System.Windows.Forms.TextBox textBoxADC1Max;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxADC2Min;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxADC2Max;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxADC1HFV;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxADC2HFV;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ RP-C-MK06/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RP_C_MK06
+{
+    public partial class Form1 : Form
+    {
+        private AdcParser _adcParser;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBoxSerialPort_DropDown(object sender, EventArgs e)
+        {
+            comboBoxSerialPort.Items.Clear();
+            foreach(string  serialPort in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                comboBoxSerialPort.Items.Add(serialPort);
+            }
+        }
+
+        private void BtnOpenPort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BtnOpenPort.Enabled = false;
+                if (comboBoxSerialPort.Text.Length == 0) {
+                    MessageBox.Show($"请先选择串口");
+                    comboBoxSerialPort.Focus();
+                    return;
+                }
+
+                // 创建 AdcParser 实例并传入 RichTextBox 控件
+                if (_adcParser == null)
+                {
+                    _adcParser = new AdcParser(comboBoxSerialPort.Text, 9600, richTextBoxLog);
+                }
+                _adcParser.Open();
+            }
+            finally
+            {
+                BtnOpenPort.Enabled = true;
+            }
+        }
+
+        private async void BtnReadADC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BtnReadADC.Enabled = false;
+                textBoxADC1Max.Text = string.Empty;
+                textBoxADC1Min.Text = string.Empty;
+                textBoxADC1HFV.Text = string.Empty;
+                textBoxADC2Max.Text = string.Empty;
+                textBoxADC2Min.Text = string.Empty;
+
+                if (_adcParser == null)
+                {
+                    MessageBox.Show($"请先打开串口");
+                    BtnOpenPort.Focus();
+                    return;
+                }
+
+                TimeSpan duration = TimeSpan.FromSeconds((double)numericUpDownDuration.Value);
+
+                List<AdcData> dataList = await _adcParser.ReadForDuration(duration);
+
+                /*Console.WriteLine($"Parsed {dataList.Count} data points:");
+                foreach (var data in dataList)
+                {
+                    Console.WriteLine($"ADC1 Value: {data.Adc1Value}, Pressed: {data.Adc1Pressed}");
+                    Console.WriteLine($"ADC2 Value: {data.Adc2Value}, Pressed: {data.Adc2Pressed}");
+                }*/
+                if (dataList == null || dataList.Count == 0)
+                {
+                    Console.WriteLine("ADC 数据列表为空。");
+                    return;
+                }
+                textBoxADC1Min.Text = dataList.Min(data=>data.Adc1Value).ToString();
+                textBoxADC1Max.Text = dataList.Max(data=>data.Adc1Value).ToString();
+
+                // 获取出现次数最多的 Adc1Value
+                var mostFrequentAdc1Value = dataList
+                    .GroupBy(data => data.Adc1Value) // 按 Adc1Value 分组
+                    .OrderByDescending(group => group.Count()) // 按出现次数降序排列
+                    .Select(group => new
+                    {
+                        Value = group.Key,
+                        Count = group.Count()
+                    })
+                    .FirstOrDefault(); // 获取出现次数最多的值
+
+                if (mostFrequentAdc1Value != null)
+                {
+                    Console.WriteLine($"出现次数最多的 Adc1Value: {mostFrequentAdc1Value.Value}, 次数: {mostFrequentAdc1Value.Count}");
+                    textBoxADC1HFV.Text = mostFrequentAdc1Value.Value.ToString();
+                }
+                else
+                {
+                    Console.WriteLine("列表为空，没有值。");
+                }
+
+                textBoxADC2Min.Text = dataList.Min(data=>data.Adc2Value).ToString();
+                textBoxADC2Max.Text = dataList.Max(data=>data.Adc2Value).ToString();
+                // 获取出现次数最多的 Adc1Value
+                var mostFrequentAdc2Value = dataList
+                    .GroupBy(data => data.Adc2Value) // 按 Adc1Value 分组
+                    .OrderByDescending(group => group.Count()) // 按出现次数降序排列
+                    .Select(group => new
+                    {
+                        Value = group.Key,
+                        Count = group.Count()
+                    })
+                    .FirstOrDefault(); // 获取出现次数最多的值
+
+                if (mostFrequentAdc1Value != null)
+                {
+                    Console.WriteLine($"出现次数最多的 Adc2Value: {mostFrequentAdc2Value.Value}, 次数: {mostFrequentAdc2Value.Count}");
+                    textBoxADC2HFV.Text = mostFrequentAdc2Value.Value.ToString();
+                }
+                else
+                {
+                    Console.WriteLine("列表为空，没有值。");
+                }
+            }
+            finally
+            {
+                BtnReadADC.Enabled = true;
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _adcParser?.Dispose();
+        }
+
+        private void BtnClosePort_Click(object sender, EventArgs e)
+        {
+            _adcParser?.Close();
+        }
+    }
+}
